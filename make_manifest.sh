d60714bf3356673d4122382a2bef337af29c5ad2
Jacky Jiang <t83714@gmail.com>
2023-10-08 16:22:39 +1100

    build to magda repo

--- make_manifest.sh
+++ make_manifest.sh
@@ -4,15 +4,15 @@ export USR=$DOCKER_NS
 export TAG=$TRAVIS_TAG
 
 docker manifest create $USR/of-watchdog:$TAG \
-  $USR/of-watchdog:$TAG-darwin \
-  $USR/of-watchdog:$TAG-x86_64 \
-  $USR/of-watchdog:$TAG-armhf \
-  $USR/of-watchdog:$TAG-arm64 \
-  $USR/of-watchdog:$TAG-windows
+  $USR/of-watchdog:latest-dev-darwin \
+  $USR/of-watchdog:latest-dev-x86_64 \
+  $USR/of-watchdog:latest-dev-armhf \
+  $USR/of-watchdog:latest-dev-arm64 \
+  $USR/of-watchdog:latest-dev-windows
 
-docker manifest annotate $USR/of-watchdog:$TAG --arch arm $USR/of-watchdog:$TAG-darwin
-docker manifest annotate $USR/of-watchdog:$TAG --arch arm $USR/of-watchdog:$TAG-armhf
-docker manifest annotate $USR/of-watchdog:$TAG --arch arm64 $USR/of-watchdog:$TAG-arm64
-docker manifest annotate $USR/of-watchdog:$TAG --os windows $USR/of-watchdog:$TAG-windows
+docker manifest annotate $USR/of-watchdog:$TAG --arch arm $USR/of-watchdog:latest-dev-darwin
+docker manifest annotate $USR/of-watchdog:$TAG --arch arm $USR/of-watchdog:latest-dev-armhf
+docker manifest annotate $USR/of-watchdog:$TAG --arch arm64 $USR/of-watchdog:latest-dev-arm64
+docker manifest annotate $USR/of-watchdog:$TAG --os windows --arch amd64 $USR/of-watchdog:latest-dev-windows
 
 docker manifest push $USR/of-watchdog:$TAG
